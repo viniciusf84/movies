d4d346742c275c67fa006ccdcfe0b3ed72c092ec
inicius <viniciusf84@hotmail.com>
2020-08-14 20:46:28 -0300

    Home function component

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 // Containers
 import SearchObs from '../Search';
 
-class Home extends Component {
-	render() {
-		return (
-			<section id="home" className="home">
-				<main className="main-content">
-					<SearchObs title="Search for your favorite movie" />
-				</main>
-			</section>
-		);
-	}
+function Home() {
+	return (
+		<section id="home" className="home">
+			<main className="main-content">
+				<SearchObs title="Search for your favorite movie" />
+			</main>
+		</section>
+	);
 }
 
 export default Home;
