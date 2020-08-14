92652b25c6bbb95b88952315650168a79d741f90
inicius <viniciusf84@hotmail.com>
2020-08-14 20:46:10 -0300

    SearchInput function component

--- src/components/General/SearchInput/SearchInput.js
+++ src/components/General/SearchInput/SearchInput.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
-class SearchInput extends Component {
+function SearchInput(props) {
+	const { name, placeholder, onChange } = props;
 
-    render() {
-        const { name, placeholder, onChange } = this.props;
-
-        return(
-            <React.Fragment>                
-                <input
-                    type="text"                                   
-                    className='form-control'
-                    name={name}
-                    placeholder={placeholder}
-                    onChange={onChange}
-                    autoComplete="off"
-                />
-                <span className='icon__wrapper'>
-                    <FontAwesomeIcon className="end-xs" icon={faSearch} size='2x'/>
-                </span>
-            </React.Fragment>
-        )
-    }
+	return (
+		<>
+			<input
+				type="text"
+				className="form-control"
+				name={name}
+				placeholder={placeholder}
+				onChange={onChange}
+				autoComplete="off"
+			/>
+			<span className="icon__wrapper">
+				<FontAwesomeIcon className="end-xs" icon={faSearch} size="2x" />
+			</span>
+		</>
+	);
 }
 
 export default SearchInput;
