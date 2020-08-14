dcda0caa2d11018c6c79ea270a6f3e7e687762bc
inicius <viniciusf84@hotmail.com>
2020-08-14 20:43:13 -0300

    ScrollToTop withRouter fix

--- src/utils/ScrollToTop.js
+++ src/utils/ScrollToTop.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 
 function ScrollToTop(props) {
@@ -6,7 +6,7 @@ function ScrollToTop(props) {
 		window.scrollTo(0, 0);
 	}, [props.location]);
 
-	return <withRouter>{props.children}</withRouter>;
+	return props.children;
 }
 
 export default withRouter(ScrollToTop);
