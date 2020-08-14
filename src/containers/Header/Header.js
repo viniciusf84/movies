ca63019774a93c38fd518437100f63c5e6cda7f4
inicius <viniciusf84@hotmail.com>
2020-08-14 18:49:17 -0300

    Header refactoring

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = props => {
+const Header = (props) => {
+	const { pageTitle } = props;
 
-    const { pageTitle } = props;
-        
-    return(
-        <header id="header" className="page-header">
-            <div className="brand start-xs">
-                <h1 className="page-title">
-                    <Link to="/">
-                        {pageTitle}
-                    </Link>
-                </h1>
-            </div>    
-        </header> 
-    )
-}
+	return (
+		<header id="header" className="page-header">
+			<div className="brand start-xs">
+				<h1 className="page-title">
+					<Link to="/">{pageTitle}</Link>
+				</h1>
+			</div>
+		</header>
+	);
+};
 
 export default Header;
