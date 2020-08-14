cb06186ce6ba8008b42976924b6d51e3bae8826e
inicius <viniciusf84@hotmail.com>
2020-08-14 18:46:47 -0300

    Results refactoring

--- src/containers/Results/Results.js
+++ src/containers/Results/Results.js
@@ -1,47 +1,41 @@
-import React, { Component } from "react";
+import React from 'react';
 
 // Components
-import LoadingContent from "../../components/DataDisplay/LoadingContent";
-import Item from "./Item";
+import LoadingContent from '../../components/DataDisplay/LoadingContent';
+import Item from './Item';
 
-class Results extends Component {
-  goTo(id) {
-    this.props.history.push(`/profile/${id}`);
-  }
+function Results(props) {
+	const { loading, movies, title, more, onClickButton } = props;
 
-  render() {
-    const { loading, movies, title, more, onClickButton } = this.props;
+	return (
+		<div className="wrapper container-fluid">
+			<h4>{title}</h4>
+			<section id="results" className="result-list">
+				<div className="row">
+					{movies &&
+						movies.length > 0 &&
+						movies.map((movie) => (
+							<Item
+								key={movie.imdbID}
+								id={movie.imdbID}
+								image={movie.Poster}
+								title={movie.Title}
+							/>
+						))}
+				</div>
+			</section>
 
-    return (
-      <div className="wrapper container-fluid">
-        <h4>{title}</h4>
-        <section id="results" className="result-list">
-          <div className="row">
-            {movies &&
-              movies.length > 0 &&
-              movies.map(movie => (
-                <Item
-                  key={movie.imdbID}
-                  id={movie.imdbID}
-                  image={movie.Poster}
-                  title={movie.Title}
-                />
-              ))}
-          </div>
-        </section>
+			{loading && (
+				<LoadingContent isLoading={loading} loadingText="Loading results" />
+			)}
 
-        {loading && (
-          <LoadingContent isLoading={loading} loadingText="Loading results" />
-        )}
-
-        {more && (
-          <button id="more" className="load-more" onClick={onClickButton}>
-            + movies...
-          </button>
-        )}
-      </div>
-    );
-  }
+			{more && (
+				<button id="more" className="load-more" onClick={onClickButton}>
+					+ movies...
+				</button>
+			)}
+		</div>
+	);
 }
 
 export default Results;
