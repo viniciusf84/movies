9be1e5308119dae4c7bd7833881c4426a9181731
inicius <viniciusf84@hotmail.com>
2020-08-14 18:48:01 -0300

    Results item refactoring

--- src/containers/Results/Item.js
+++ src/containers/Results/Item.js
@@ -1,34 +1,38 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
+import { SearchContext } from '../../contexts/SearchContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFilm } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
-class Item extends Component {
-    
-    render() {
-        const { id, image, title } = this.props
+function Item(props) {
+	const { id, image, title } = props;
+	const searchContext = useContext(SearchContext);
+	const { setUpdateList } = searchContext.actions;
 
-        return (
-            <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-                <div className="result-list__item">
-                    <Link to={`/profile/${id}`}>
-                        <div className={image === 'N/A' ? 'wrap icon__wrapper' : 'wrap  img__wrapper' } >
-                            {image === 'N/A' ?
-                                <FontAwesomeIcon icon={faFilm} size="10x" />
-                            :                                            
-                                <img src={image} alt={title} />
-                            }
-                        </div>
-                    </Link>
-                    <div className="title">
-                        <Link to={`/profile/${id}`}>
-                            <h5>{title}</h5>
-                        </Link>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+	return (
+		<div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+			<div className="result-list__item" onClick={() => setUpdateList(false)}>
+				<Link to={`/profile/${id}`}>
+					<div
+						className={
+							image === 'N/A' ? 'wrap icon__wrapper' : 'wrap  img__wrapper'
+						}
+					>
+						{image === 'N/A' ? (
+							<FontAwesomeIcon icon={faFilm} size="10x" />
+						) : (
+							<img src={image} alt={title} />
+						)}
+					</div>
+				</Link>
+				<div className="title">
+					<Link to={`/profile/${id}`}>
+						<h5>{title}</h5>
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Item;
