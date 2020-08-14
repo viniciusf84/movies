4432ac14f16ba864f53f976e60d83fbb35364c36
inicius <viniciusf84@hotmail.com>
2020-08-14 18:48:46 -0300

    App refactoring

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,33 +1,35 @@
-import React, { useContext } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 // Containers
-import Header from "../Header";
-import Footer from "../Footer";
+import Header from '../Header';
+import Footer from '../Footer';
 
 // Pages
-import Home from "../Home";
-import Profile from "../Profile";
+import Home from '../Home';
+import Profile from '../Profile';
 
 // utils
-import ScrollToTop from "../../utils/ScrollToTop";
+import ScrollToTop from '../../utils/ScrollToTop';
 
 // Store
-import { SearchContextProvider } from "../../stores/SearchStore";
+import { SearchContextProvider } from '../../contexts/SearchContext';
 
 const App = () => {
-  return (
-    <SearchContextProvider>
-      <Router>
-        <ScrollToTop>
-          <Header pageTitle="TELAFILME" />
-          <Route exact path="/" component={Home} />
-          <Route path="/profile/:movieId" component={Profile} />
-          <Footer text="TelaFilme 2019 - Todos os direitos reservados" />
-        </ScrollToTop>
-      </Router>
-    </SearchContextProvider>
-  );
+	return (
+		<SearchContextProvider>
+			<Router>
+				<ScrollToTop>
+					<Header pageTitle="TELAFILME" />
+					<Route exact path="/" component={Home} />
+					<Route path="/profile/:movieId" component={Profile} />
+					<Footer
+						text={`TelaFilme ${new Date().getFullYear()} - Todos os direitos reservados`}
+					/>
+				</ScrollToTop>
+			</Router>
+		</SearchContextProvider>
+	);
 };
 
 export default App;
