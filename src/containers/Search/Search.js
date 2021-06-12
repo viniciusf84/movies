eeedbe7e583528d2c47d67f37b0ba9a12f8e446c
inicius <viniciusf84@hotmail.com>
2021-06-12 15:34:38 -0300

    Search refactor

--- src/containers/Search/Search.js
+++ src/containers/Search/Search.js
@@ -1,29 +1,16 @@
-import React, { useState, useEffect, useContext, useCallback } from 'react';
+import React, { useContext, useCallback } from 'react';
 import { debounce } from 'lodash';
 import { SearchContext } from '../../contexts/SearchContext';
 
 // components
 import SearchInput from '../../components/General/SearchInput';
 
-// containers
-import Results from '../Results';
-
-// service
-import { titleSearch } from '../../utils/services';
-
-export default function Search(props) {
-	const [message, setMessage] = useState('');
-	const [loading, setLoading] = useState(false);
-	const [results, setResults] = useState([]);
-	const [totalResults, setTotalResults] = useState(null);
+export default function Search() {
 	const searchContext = useContext(SearchContext);
-	const { data, search, page, updateList, displayMoreButton } = searchContext;
 	const {
-		setSearchData,
 		setSearchString,
-		setSearchPage,
 		setUpdateList,
-		setDisplayMoreButton,
+    setMessage,
 	} = searchContext.actions;
 
 	const onHandleChange = useCallback(
@@ -37,105 +24,13 @@ export default function Search(props) {
 		[],
 	);
 
-	const onMovieClick = useCallback(() => {
-		setUpdateList(true);
-		setSearchPage(page + 1);
-	}, [page]);
-
-	const getResults = useCallback(async (searchStr, pageNumber) => {
-		setLoading(true);
-
-		try {
-			const response = await titleSearch(searchStr, pageNumber);
-
-			if (response.data.Response === 'False') {
-				// error
-				setResults([]);
-				setMessage(response.data.Error);
-			} else {
-				// success
-				setResults(response.data.Search);
-				setMessage(`Results for "${searchStr}"`);
-				setTotalResults(parseInt(response.data.totalResults));
-			}
-		} catch (error) {
-			setMessage(error);
-		}
-
-		setLoading(false);
-	}, []);
-
-	useEffect(() => {
-		if (data && data.length > 0) {
-			// brings data from context
-
-			setMessage(`Results for "${search}"`);
-		} else {
-			setSearchString('Batman');
-		}
-	}, []);
-
-	useEffect(() => {
-		if (search === '') {
-			setMessage('');
-		}
-
-		if (search.length > 0 && updateList) {
-			setSearchData([]);
-			setMessage('Loading');
-
-			if (page === 1) {
-				getResults(search, 1);
-			} else {
-				setSearchPage(1);
-			}
-		}
-	}, [search]);
-
-	useEffect(() => {
-		if (page && updateList) {
-			getResults(search, page);
-		}
-	}, [page]);
-
-	useEffect(() => {
-		if (results.length > 0) {
-			if (page === 1) {
-				setSearchData(results);
-			} else {
-				setSearchData([...data, ...results]);
-			}
-		}
-	}, [results]);
-
-	useEffect(() => {
-		if (data.length > 0 && totalResults) {
-			setDisplayMoreButton(totalResults > data.length);
-		}
-	}, [data, totalResults]);
-
 	return (
-		<>
-			<section className="search-form">
-				<div className="wrapper container-fluid">
-					<h2>{props.title}</h2>
-
-					<SearchInput
-						name="movie-search-input"
-						placeholder="Type here..."
-						onChange={(e) => onHandleChange(e.target.value)}
-					/>
-				</div>
-			</section>
-
-			<Results
-				loading={loading}
-				search={search}
-				movies={data}
-				title={message}
-				more={displayMoreButton}
-				onClickButton={() => onMovieClick()}
-			/>
-		</>
+		<section className="search-form">						
+      <SearchInput
+        name="movie-search-input"
+        placeholder="Search for your favorite movie"
+        onChange={(e) => onHandleChange(e.target.value)}
+      />			
+    </section>
 	);
 }
