c8aad4b58edc2b2e21378c9da730560edd75ca82
inicius <viniciusf84@hotmail.com>
2021-06-12 15:28:54 -0300

    Isolate search input component

--- src/components/General/SearchInput/SearchInput.js
+++ src/components/General/SearchInput/SearchInput.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+// import './_searchinput.scss';
 
 function SearchInput(props) {
 	const { name, placeholder, onChange } = props;
