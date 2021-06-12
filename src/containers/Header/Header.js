eb863e34d3002ce51f1bb5c5f3f1fa7cd6e8533c
inicius <viniciusf84@hotmail.com>
2021-06-12 15:29:53 -0300

    Header imports search component

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = (props) => {
-	const { pageTitle } = props;
+// components 
+import Search from '../Search';
+
+const Header = ({ pageTitle }) => {
 
 	return (
 		<header id="header" className="page-header">
-			<div className="brand start-xs">
-				<h1 className="page-title">
-					<Link to="/">{pageTitle}</Link>
-				</h1>
-			</div>
+      <div className="wrapper container-fluid">
+        <div className="row">
+          <div className="col-xs-12 col-sm-6 col-md-6 col-lg-4">  
+            <div className="brand start-xs">
+              <h1 className="page-title">
+                <Link to="/">{pageTitle}</Link>
+              </h1>
+            </div>
+          </div>
+
+          <div className="col-xs-12 col-sm-6 col-md-6 col-lg-8">
+            <div className="search-wrapper">
+              <Search />
+            </div>
+          </div>
+        </div>
+      </div>
 		</header>
 	);
 };
